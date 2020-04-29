fba0b31d0ecc8955efe9426863bfef9559a6e619
Kuba Sekowski <sequba@gmail.com>
2020-04-29 17:53:38 +0200

    Create markup for beer description component

--- src/app/beers/beer-details/beer-description/beer-description.component.ts
+++ src/app/beers/beer-details/beer-description/beer-description.component.ts
@@ -6,16 +6,34 @@ import { Beer } from 'src/app/dtos/beer.dto';
   template: `
     <div class="row">
       <div class="col-2 col-sm-3 col-lg-2">
-        <img class="img-fluid" src="{{ beer!.image_url }}" alt="Photo of the beer">
+        <img class="img-fluid mb-3" src="{{ beer!.image_url }}" alt="Photo of the beer">
       </div>
       <div class="col-10 col-sm-9 col-lg-10">
-        <h5 class="modal-title">{{ beer!.name }}</h5>
+        <h4 class="modal-title">{{ beer!.name }}</h4>
+        <h6 class="modal-subtitle text-muted">{{ beer!.tagline }}</h6>
+        <div class="border-top border-primary"></div>
+        <p>
+          <span class="mr-3"><span class="font-weight-bold">IBU:</span> {{ beer!.ibu }}</span>
+          <span class="mr-3"><span class="font-weight-bold">ABV:</span> {{ beer!.abv }}</span>
+          <span class="mr-3"><span class="font-weight-bold">EBC:</span> {{ beer!.ebc }}</span>
+        </p>
         <p>{{ beer!.description }}</p>
+        <ng-container *ngIf="beer && beer.food_pairing && beer.food_pairing.length > 0">
+          <p class="font-weight-bold">Best served with:</p>
+          <ul>
+            <li *ngFor="let food of beer.food_pairing">{{ food }}</li>
+          </ul>
+        </ng-container>
       </div>
     </div>
   `,
-  styles: [
-  ]
+  styles: [`
+    .border-primary {
+      height: 2rem;
+      width: 4rem;
+      border-width: 0.2rem !important;
+    }
+  `]
 })
 export class BeerDescriptionComponent implements OnInit {
   @Input() beer: Beer | undefined;
