268a97337f90be1c93c9aae25a9d73882990bf72
Kuba Sekowski <sequba@gmail.com>
2020-04-30 21:38:03 +0200

    Add filtering to the api service

--- src/app/punk-api/punk-api.service.ts
+++ src/app/punk-api/punk-api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Beer } from '../dtos/beer.dto';
 import { map } from 'rxjs/operators';
+import { PunkFilters } from './punk-filters';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,16 @@ import { map } from 'rxjs/operators';
 export class PunkApiService {
   readonly rootUrl = 'https://api.punkapi.com/v2/beers';
 
-  fetchBeers(pageNo: number, perPage: number): Observable<Beer[]> {
-    const url = `${this.rootUrl}?page=${pageNo}&per_page=${perPage}`;
-    return this.http.get<Beer[]>(url);
+  constructor(private http: HttpClient) { }
+
+  fetchBeers(pageNo: number, perPage: number, filters: PunkFilters = {}): Observable<Beer[]> {
+    const url = `${this.rootUrl}`;
+    const params = {...filters, page: pageNo, per_page: perPage} as unknown as {[key: string]: string};
+    return this.http.get<Beer[]>(url, {params});
   }
 
   fetchBeerById(id: number): Observable<Beer> {
     const url = `${this.rootUrl}/${id}`;
     return this.http.get<Beer[]>(url).pipe(map(res => res[0]));
   }
-
-  constructor(private http: HttpClient) { }
 }
---
+++ src/app/punk-api/punk-filters.ts
@@ -0,0 +1,8 @@
+export interface PunkFilters {
+  abv_gt?: number;
+  abv_lt?: number;
+  ibu_gt?: number;
+  ibu_lt?: number;
+  ebc_gt?: number;
+  ebc_lt?: number;
+}
