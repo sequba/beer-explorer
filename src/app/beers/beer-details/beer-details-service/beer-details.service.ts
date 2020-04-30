f93c10bb1e4a4f893ffd75f99223005c4394dda0
Kuba Sekowski <sequba@gmail.com>
2020-04-30 21:39:34 +0200

    Fetch related beers using  the ibu, abv & ebc filtering

--- src/app/beers/beer-details/beer-details-service/beer-details.service.ts
+++ src/app/beers/beer-details/beer-details-service/beer-details.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 import { PunkApiService } from 'src/app/punk-api/punk-api.service';
 import { Beer } from 'src/app/dtos/beer.dto';
 import { Observable, of } from 'rxjs';
+import { PunkFilters } from 'src/app/punk-api/punk-filters';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerDetailsService {
+  readonly ibuDelta = 5;
+  readonly abvDelta = 2;
+  readonly ebcDelta = 18;
 
   constructor(private apiService: PunkApiService) { }
 
@@ -15,6 +20,14 @@ export class BeerDetailsService {
   }
 
   getRelatedBeers(beer: Beer): Observable<Beer[]> {
-    return of([beer, beer, beer]);
+    const filters: PunkFilters = {
+      ibu_gt: Math.max(beer.ibu - this.ibuDelta, 0), ibu_lt: beer.ibu + this.ibuDelta,
+      abv_gt: Math.max(beer.abv - this.abvDelta, 0), abv_lt: beer.abv + this.abvDelta,
+      ebc_gt: Math.max(beer.ebc - this.ebcDelta, 0), ebc_lt: beer.ebc + this.ebcDelta
+    };
+    return this.apiService.fetchBeers(1, 4, filters).pipe(
+      map(related => related.filter(b => b.id !== beer.id)),
+      map(beers => beers.slice(0, 3))
+    );
   }
 }
