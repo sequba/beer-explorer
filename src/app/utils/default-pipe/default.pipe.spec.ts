f04c958eef3ae708c5e85549e46371f16b427bb4
Kuba Sekowski <sequba@gmail.com>
2020-05-03 16:39:37 +0200

    Tests for DefaultPipe

--- src/app/utils/default-pipe/default.pipe.spec.ts
+++ src/app/utils/default-pipe/default.pipe.spec.ts
@@ -5,4 +5,36 @@ describe('DefaultPipe', () => {
     const pipe = new DefaultPipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('transform', () => {
+    it('shuold return value when value is a non-empty string', () => {
+      const pipe = new DefaultPipe();
+      expect(pipe.transform('string', 'default')).toEqual('string');
+    });
+
+    it('shuold return value when value is an empty string', () => {
+      const pipe = new DefaultPipe();
+      expect(pipe.transform('', 'default')).toEqual('');
+    });
+
+    it('shuold return value when value is a non-zero number', () => {
+      const pipe = new DefaultPipe();
+      expect(pipe.transform(42, 'default')).toEqual(42);
+    });
+
+    it('shuold return value when value is the number "0"', () => {
+      const pipe = new DefaultPipe();
+      expect(pipe.transform(0, 'default')).toEqual(0);
+    });
+
+    it('shuold return defaultValue when value is undefined', () => {
+      const pipe = new DefaultPipe();
+      expect(pipe.transform(undefined, 'default')).toEqual('default');
+    });
+
+    it('shuold return value when value is null', () => {
+      const pipe = new DefaultPipe();
+      expect(pipe.transform(null, 'default')).toEqual('default');
+    });
+  });
 });
--- src/app/utils/default-pipe/default.pipe.ts
+++ src/app/utils/default-pipe/default.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'default'
 })
 export class DefaultPipe implements PipeTransform {
-  transform(value: string | number | null, defaultValue: string): string | number {
+  transform(value: string | number | null | undefined, defaultValue: string | number): string | number {
     return (value !== null && value !== undefined) ? value : defaultValue;
   }
 }
